a17d5bedea2f8b8efb530f00c4d89f8f0e111469
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-04-08 16:11:31 +0400

    commit-006 - add spinner overlay store for future spinners

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import { events } from "./Events";
+import { spinnerOverlay } from "./SpinnerOverlay";
 
-const reducers = combineReducers({ events });
+const reducers = combineReducers({ events, spinnerOverlay });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
