a4dba375dad82ae3cad7b1c2fe9536b847ebed37
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-04-13 23:16:19 +0400

    commit-028 - fixed bug with row selection

--- src/Components/EventsTable/Components/EventRow/EventRow.jsx
+++ src/Components/EventsTable/Components/EventRow/EventRow.jsx
@@ -21,35 +21,49 @@ export const EventRow = ({ id, isSelectedRow }) => {
 
 	const markup = useMemo(() =>
 	(<div
-		className={isSelectedRow ? 'events-table-row selected-row' : 'events-table-row'}
+		className='events-table-row'
 		onClick={handleSelectionOnRow}
 		>
 
-		{!!checkboxControl[0] && <div className='row-item column-start-time-date' style={{ width: `${columnsWidth[0]}px` }}>
+		{!!checkboxControl[0] && <div
+			className={isSelectedRow ? 'row-item column-start-time-date selected-row' : 'row-item column-start-time-date'}
+				style={{ width: `${columnsWidth[0]}px` }}>
 			<span className='start-time-date'>{startTimeDate}</span>
 		</div>}
 
-		{!!checkboxControl[1] && <div className='row-item column-end-time-date' style={{ width: `${columnsWidth[1]}px` }}>
+		{!!checkboxControl[1] && <div
+			className={isSelectedRow ? 'row-item column-end-time-date selected-row' : 'row-item column-end-time-date'}
+				style={{ width: `${columnsWidth[1]}px` }}>
 			<span className='end-time-date'>{endTimeDate}</span>
 		</div>}
 
-		{!!checkboxControl[2] && <div className='row-item column-description' style={{ width: `${columnsWidth[2]}px` }}>
+		{!!checkboxControl[2] && <div
+			className={isSelectedRow ? 'row-item column-description selected-row' : 'row-item column-description'}
+				style={{ width: `${columnsWidth[2]}px` }}>
 			<span className='description'>{description}</span>
 		</div>}
 
-		{!!checkboxControl[3] && <div className='row-item column-device' style={{ width: `${columnsWidth[3]}px` }}>
+		{!!checkboxControl[3] && <div
+			className={isSelectedRow ? 'row-item column-device selected-row' : 'row-item column-device'}
+				style={{ width: `${columnsWidth[3]}px` }}>
 			<span className='device'>{device}</span>
 		</div>}
 
-		{!!checkboxControl[4] && <div className='row-item column-zone-of-device' style={{ width: `${columnsWidth[4]}px` }}>
+		{!!checkboxControl[4] && <div
+			className={isSelectedRow ? 'row-item column-zone-of-device selected-row' : 'row-item column-zone-of-device'}
+				style={{ width: `${columnsWidth[4]}px` }}>
 			<span className='zone-of-device'>{zoneOfDevice}</span>
 		</div>}
 
-		{!!checkboxControl[5] && <div className='row-item column-color-code' style={{ width: `${columnsWidth[5]}px` }}>
+		{!!checkboxControl[5] && <div
+			className={isSelectedRow ? 'row-item column-color-code selected-row' : 'row-item column-color-code'}
+				style={{ width: `${columnsWidth[5]}px` }}>
 			<span className='color-code'>{colorCode}</span>
 		</div>}
 
-		{!!checkboxControl[6] && <div className='row-item column-action-to-do' style={{ width: `${columnsWidth[6]}px` }}>
+		{!!checkboxControl[6] && <div
+			className={isSelectedRow ? 'row-item column-action-to-do selected-row' : 'row-item column-action-to-do'}
+				style={{ width: `${columnsWidth[6]}px` }}>
 			<span className='action-to-do'>{actionToDo}</span>
 		</div>}
 
